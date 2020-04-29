94e4e91745ca33a6cab71da7b362bfe1083ba0ba
Kush Baheti <kushbaheti@gmail.com>
2020-04-29 13:24:22 +0530

    Setup startRemoveExpense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,6 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
+import { startAddExpense, addExpense, editExpense, removeExpense, setExpenses, startSetExpenses, startRemoveExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
@@ -21,6 +21,25 @@ test('should setup remove expense action object', () => {
     id: '123abc'
   });
 });
+
+test('Should remove expense from firebase', (done) => {
+  const store = createMockStore({})
+  const id = expenses[2].id
+
+  store.dispatch(startRemoveExpense({ id })).then(() => {
+    const actions = store.getActions()
+
+    expect(actions[0]).toEqual({
+      type: 'REMOVE_EXPENSE',
+      id
+    })
+
+    return database.ref(`expenses/${id}`).once('value')
+  }).then((snapshot) => {
+    expect(snapshot.val()).toBeFalsy()
+    done()
+  })
+})
 
 test('should setup edit expense action object', () => {
   const action = editExpense('123abc', { note: 'New note value' });
@@ -103,7 +122,7 @@ test('should setup set expense action object with data', () => {
 
 test('Should fetch data from database', (done) => {
   const store = createMockStore({})
-  store.dispatch(startSetExpenses).then(() => {
+  store.dispatch(startSetExpenses()).then(() => {
     const actions = store.getActions()
 
     expect(actions[0]).toEqual({
