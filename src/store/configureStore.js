dca9a362d379e264dbe335e9b745aef1094627a5
Kush Baheti <kushbaheti@gmail.com>
2020-05-28 20:52:00 +0530

    Add private routes

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,8 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import expensesReducer from '../reducers/expenses'
 import filtersReducer from '../reducers/filters'
-import thunk from 'redux-thunk'
+import authReducer from '../reducers/auth'
 
 export default () => {
     // Store creation
@@ -9,7 +10,8 @@ export default () => {
     const store = createStore(
         combineReducers({
             expenses: expensesReducer,
-            filters: filtersReducer
+            filters: filtersReducer,
+            auth: authReducer
         }),
         applyMiddleware(thunk)
     )
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import toJson from 'enzyme-to-json' 
-import Header from '../../components/Header'
+import { Header } from '../../components/Header'
 
 test('Should render Header correctly', () => {
-    const wrapper = shallow(<Header />)
+    const wrapper = shallow(<Header startLogout={() => {}} />)
     // expect(toJson(wrapper)).toMatchSnapshot()
     expect(wrapper).toMatchSnapshot()
 
@@ -12,4 +12,11 @@ test('Should render Header correctly', () => {
     // const renderer = new ReactShallowRenderer()
     // renderer.render(<Header />)
     // expect(renderer.getRenderOutput()).toMatchSnapshot()
+})
+
+test('Should call startLogout on button click', () => {
+    const startLogout = jest.fn()
+    const wrapper = shallow(<Header startLogout={startLogout} />)
+    wrapper.find('button').simulate('click')
+    expect(startLogout).toHaveBeenCalled()
 })
